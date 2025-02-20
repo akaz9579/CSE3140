434d6705c021c2ba46842a32faf8bcdc83f37290
akaz9579 <akaz9579@gmail.com>
2025-02-20 14:53:51 -0500

    Lab edits

--- Lab2/Q1A.py
+++ Lab2/Q1A.py
@@ -5,12 +5,13 @@ os.chdir('/home/cse/Lab2/LabGen')
 
 files = os.listdir('.') #list of all these files 
 
-output = "Q1A.out" #creates the out file 
+out = "Q1A.out" #creates the out file 
 
 #changes back to Solutions for it to be stored here. 
 os.chdir('/home/cse/Lab2/Solutions')
 
 #opens output file to write all file names in  
-    for f in files: 
-        out.write(f + "\n")
+for f in files: 
+    out.write(f + "\n")
+
     
